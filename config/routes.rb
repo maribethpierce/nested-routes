2a6b743e8ff3c851dbf8f4988e9471801644330c
Maribeth Pierce <maribeth.pierce@hellolabs.com>
2016-06-21 15:48:59 -0400

    add api endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,9 @@ Rails.application.routes.draw do
     resources :campers
   end
   resources :supplies, only: [:index]
+  namespace :api do
+    namespace :v1 do
+      resources :campers, only: [:index]
+    end
+  end
 end
